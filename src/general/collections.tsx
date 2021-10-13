ba4f6b87000f252f731072ed83c478e61f50fae3
ugbejohn <engr.ugbechike@gmail.com>
2021-10-13 00:45:22 -0400

    added add fn

--- src/general/collections.tsx
+++ src/general/collections.tsx
@@ -2,10 +2,14 @@ export function Collections() {
     let arr = [1, 2, 3, 4, 5]
     let a = 5
     let b = 8
-    
+
     // let me help you
 
     arr.map((item, index) => item)
 
     return 1+1
+}
+
+function addSum(a:number, b:number ){
+    return a + b
 }
