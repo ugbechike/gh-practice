7baae933eaeae7c4031c7f68c749f042285ceeff
ugbejohn <engr.ugbechike@gmail.com>
2021-10-13 00:43:24 -0400

    added array

--- src/general/collections.tsx
+++ src/general/collections.tsx
@@ -1,3 +1,11 @@
 export function Collections() {
+    let arr = [1, 2, 3, 4, 5]
+    let a = 5
+    let b = 8
+    
+    // let me help you
+
+    arr.map((item, index) => item)
+
     return 1+1
 }
